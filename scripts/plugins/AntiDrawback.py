2a23f3cb67532975ed4bf43cb78815df23ad5f20
JSYRD <61402085+JSYRD@users.noreply.github.com>
2022-09-13 20:18:53 +0800

    feat: 添加@功能

--- scripts/plugins/AntiDrawback.py
+++ scripts/plugins/AntiDrawback.py
@@ -17,7 +17,7 @@ class AntiDrawback(Plugin):
             if(self.state and event['post_type'] == 'notice' and event['notice_type'] == 'group_recall'):
                 try:
                     retJson = requests.get(CONFIG.serverUrl+'get_msg',{'message_id':event['message_id']}).json()
-                    self.PutEvent2Bot(BotEvent('group', event['group_id'], '%d 在 %s 撤回了 %d 的消息：\n%s' % (event['operator_id'], str(datetime.fromtimestamp(event['time'])), event['user_id'], retJson['data']['message'])))
+                    self.PutEvent2Bot(BotEvent('group', event['group_id'], '%s 在 %s 撤回了 %s 的消息：\n%s' % ("[CQ:at,qq=%d]" % event['operator_id'], str(datetime.fromtimestamp(event['time'])), "[CQ:at,qq=%d]" % event['user_id'], retJson['data']['message'])))
                     # self.SendGroupMsg(event['group_id'], '%d 在 %s 撤回了 %d 的消息：\n%s' % (event['operator_id'], str(datetime.fromtimestamp(event['time'])), event['user_id'], retJson['data']['message']))
                 except:
                     raise
