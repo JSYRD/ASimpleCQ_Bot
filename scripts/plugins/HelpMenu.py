ba0f166c9bf33fc592568fa69f42ac398b6b4792
JSYRD <61402085+JSYRD@users.noreply.github.com>
2022-09-13 20:35:54 +0800

    feat: 添加输出doc功能

--- scripts/plugins/HelpMenu.py
+++ scripts/plugins/HelpMenu.py
@@ -1,21 +1,28 @@
 import pkgutil
 from BotEvent import BotEvent
 from Plugin import Plugin
+__PluginNames__ = [name for _, name,
+                   _ in pkgutil.iter_modules(['./plugins'])]  # 获取插件类的名称
+__Plugins__ = []
+for name in __PluginNames__:
+    exec('from plugins.%s import %s' % (name, name))
+    exec('__Plugins__.append(%s())' % (name))  # 将插件加入到列表中
+
+
 class HelpMenu(Plugin):
     def __init__(self) -> None:
         super().__init__()
-        self.FunctionNames = [name for _, name, _ in pkgutil.iter_modules(['./plugins'])]
         self.ans = '当前插件：'
-        for functionName in self.FunctionNames:
-            self.ans+='\n'+functionName
-    # def CommandOperate(self, param, json):
-    #     if(len(param) > 0 and param[0]=='help'):
-    #         self.SendGroupMsg(json['group_id'], self.ans)
+        for i in len(__PluginNames__):
+            self.ans += "\n%s: %s" % (__PluginNames__[i],
+                                      __Plugins__[i].__doc__)
+
     def run(self):
-        while(True):
+        while (True):
             event = self.eventBox.get(block=True)
             param = self.__DecodeCommand__(event)
 
-            if(len(param) > 0 and param[0]=='help'):
+            if (len(param) > 0 and param[0] == 'help'):
                 # self.SendGroupMsg(json['group_id'], self.ans)
-                self.PutEvent2Bot(BotEvent('group', event['group_id'], self.ans))
+                self.PutEvent2Bot(
+                    BotEvent('group', event['group_id'], self.ans))
